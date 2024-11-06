38d5ddb9553d54462296b21064c4ba6fddb68eb8
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2024-11-06 16:35:17 -0500

    feat: instantiate and configure an axios api client for making calls to backend api
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>

---
+++ src/services/data.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+const baseUrl = import.meta.env.VITE_BASEURL;
+
+const apiClient = axios.create({
+  baseURL: baseUrl,
+  withCredentials: true,
+});
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const access_token = JSON.parse(localStorage.getItem("userData"))?.token; // localStorage is used for now
+    if (access_token) {
+      config.headers["Authorization"] = `Bearer ${access_token}`;
+      config.headers["Content-Type"] = "application/json";
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+apiClient.interceptors.response.use(
+  (response) => {
+    const newAccessToken = response.headers["new-access-token"];
+
+    if (newAccessToken) {
+      const userData = JSON.parse(localStorage.getItem("userData")) || {};
+      userData.access_token = newAccessToken;
+      localStorage.setItem("userData", JSON.stringify(userData));
+    }
+
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+    }
+
+    return Promise.reject(error);
+  }
+);
