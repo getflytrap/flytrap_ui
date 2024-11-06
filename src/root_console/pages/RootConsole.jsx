688edd92aa982cee97678b7147d270d3e6ecd048
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-06 22:31:57 +0100

    feat: add user management section and vertical divider
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- src/root_console/pages/RootConsole.jsx
+++ src/root_console/pages/RootConsole.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { Box, Button, Heading, Stack, Text } from "@chakra-ui/react";
+import { Box, Button, Divider, Heading, Stack, Text } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 const RootConsole = () => {
@@ -9,15 +8,39 @@ const RootConsole = () => {
         Admin Console
       </Heading>
 
-      <Box flex="1" p={5} textAlign="center">
-        <Heading as="h2" size="lg" mb={4}>
-          Error Dashboard
-        </Heading>
-        <Text mb={4}>Monitor and manage errors here.</Text>
-        <Link to="/">
-          <Button colorScheme="teal">Go to Dashboard</Button>
-        </Link>
-      </Box>
+      <Stack direction="row" spacing={10} align="flex-start">
+        <Box flex="1" p={5} textAlign="center">
+          <Heading as="h2" size="lg" mb={4}>
+            Error Dashboard
+          </Heading>
+          <Text mb={4}>Monitor and manage errors here.</Text>
+          <Link to="/">
+            <Button colorScheme="teal">Go to Dashboard</Button>
+          </Link>
+        </Box>
+
+        <Box>
+          <Divider
+            orientation="vertical"
+            height="100px"
+            borderColor="gray.300"
+          />
+        </Box>
+
+        <Box flex="1" p={5} textAlign="center">
+          <Heading as="h2" size="lg" mb={4}>
+            Manage Users
+          </Heading>
+          <Stack spacing={4} align="center">
+            <Text mb={4}>Create or remove users here.</Text>
+
+            <Link to="/manage-users">
+              <Button colorScheme="blue">Go To User Management Console</Button>
+            </Link>
+          </Stack>
+        </Box>
+
+      </Stack>
     </Box>
   );
 };
