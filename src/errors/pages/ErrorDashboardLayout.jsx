6aeb1c89fe2ebc0d542ca26d67bc1ae0e558619c
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2024-11-06 16:14:46 -0500

    perf: replace divs with responsive chakra ui grid components
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>

--- src/errors/pages/ErrorDashboardLayout.jsx
+++ src/errors/pages/ErrorDashboardLayout.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Grid, GridItem } from "@chakra-ui/react";
+
 import Sidebar from "../components/Sidebar";
 
 export default function ErrorDashboardLayout(props) {
   return (
     <div className="site-wrapper">
-      <div className="grid-container">
-        <aside className="sidebar">
+      <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
+        <GridItem
+          as="aside"
+          colSpan={{ base: 6, lg: 2, xl: 1 }}
+          bg="purple.400"
+          minHeight={{ lg: "100vh" }}
+          p={{ base: "20px", lg: "30px" }}
+        >
           <Sidebar {...props} />
-        </aside>
+        </GridItem>
 
-        <main className="content">
+        <GridItem as="main" colSpan={{ base: 6, lg: 4, xl: 5 }} p="40px">
           <Outlet />
-        </main>
-      </div>
+        </GridItem>
+      </Grid>
     </div>
   );
 }
