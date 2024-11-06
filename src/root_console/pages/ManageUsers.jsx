c63324d8655894b8c287bbb083b78594d08f2ede
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-06 22:37:35 +0100

    feat: add user fetching logic to user management component
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- src/root_console/pages/ManageUsers.jsx
+++ src/root_console/pages/ManageUsers.jsx
@@ -1,8 +1,24 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { ArrowBackIcon } from "@chakra-ui/icons";
 import { Box, Button, Heading } from "@chakra-ui/react";
+import { getUsers } from "../../services/data";
 
 const ManageUsers = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      const data = await getUsers();
+      setUsers(data.users);
+    }
+    try {
+      fetchUsers();
+    } catch {
+      alert("Could not fetch users");
+    }
+  }, []);
+
   return (
     <Box p={5}>
       <Box textAlign="left" mb={6}>
