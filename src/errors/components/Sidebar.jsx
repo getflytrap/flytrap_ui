0695c3ff525cd30ea6752c60c168e5a9c8891619
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2024-11-06 16:23:24 -0500

    feat: sidebar list items respond to user click to display corresponding errors in the errors table
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>

--- src/errors/components/Sidebar.jsx
+++ src/errors/components/Sidebar.jsx
@@ -1,30 +1,79 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Box, Stack, Text, Button, HStack } from "@chakra-ui/react";
+
+import { getAllProjects } from "../../services/data";
+
+const PROJECT_LIMIT_PER_PAGE = 10;
 
 export default function Sidebar({ selectedProject, setSelectedProject }) {
-    const navigate = useNavigate();
-  
-    const [loadedProjects, setLoadedProjects] = useState([]);
-  
-    useEffect(() => {
-      fetchProjects();
-    }, []);
-  
-    async function fetchProjects(pageToRequest = 1) {
-      try {
-        const data = await getAllProjects(pageToRequest, PROJECT_LIMIT_PER_PAGE);
-        console.log(data.projects);
-  
-        setLoadedProjects(data.projects);
-        setCurrentPage(data.current_page);
-        setTotalPages(data.total_pages);
-      } catch (e) {
-        alert("Couldn't fetch project data");
-      }
+  const navigate = useNavigate();
+
+  const [loadedProjects, setLoadedProjects] = useState([]);
+
+  useEffect(() => {
+    fetchProjects();
+  }, []);
+
+  async function fetchProjects(pageToRequest = 1) {
+    try {
+      const data = await getAllProjects(pageToRequest, PROJECT_LIMIT_PER_PAGE);
+      console.log(data.projects);
+
+      setLoadedProjects(data.projects);
+      setCurrentPage(data.current_page);
+      setTotalPages(data.total_pages);
+    } catch (e) {
+      alert("Couldn't fetch project data");
     }
-    return (
-        <ul>
-            {loadedProjects}
-        </ul>
-    )
+  }
 
+  const handleClick = (project) => {
+    setSelectedProject(project);
+    navigate("/errors");
+  };
 
+  return (
+    <>
+      <Stack spacing={3} color="whiteAlpha.900">
+        {loadedProjects.map((project) => (
+          <Box
+            onClick={() => handleClick(project)}
+            key={project.project_id}
+            borderRadius="20px"
+            bg={
+              selectedProject.project_id === project.project_id
+                ? "blue.100"
+                : "transparent"
+            }
+            border={
+              selectedProject.project_id === project.project_id ? "2px" : "1px"
+            }
+            borderColor={
+              selectedProject.project_id === project.project_id
+                ? "blue.500"
+                : "transparent"
+            }
+            p={2}
+            _hover={{
+              borderColor: "gray.300",
+            }}
+            cursor="pointer"
+            color={
+              selectedProject.project_id === project.project_id
+                ? "black"
+                : "inherit"
+            }
+            fontWeight={
+              selectedProject.project_id === project.project_id
+                ? "bold"
+                : "normal"
+            }
+          >
+            <Text textAlign="left">{`${project.project_name} (${project.issue_count})`}</Text>
+          </Box>
+        ))}
+      </Stack>
+    </>
+  );
+}
