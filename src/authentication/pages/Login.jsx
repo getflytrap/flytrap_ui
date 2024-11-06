4dfa604355358b36962e668ba441fa575cbb1f59
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-06 22:05:10 +0100

    feat: added component return value
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>

--- src/authentication/pages/Login.jsx
+++ src/authentication/pages/Login.jsx
@@ -27,6 +27,86 @@ const Login = () => {
   const [userIsRootUser, setUserIsRootUser] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  return (
+    <>
+      <Heading as="h1" size="xl">
+        Welcome to Fly Trap!
+      </Heading>
+      <Text
+        fontSize="xl"
+        m="50px"
+        border="1px dashed gray"
+        borderRadius="100px"
+        p="30px"
+        bg="gray.100"
+      >
+        LOGO
+      </Text>
+      <Box
+        borderWidth="1px"
+        borderColor="lightgray"
+        p={6}
+        borderRadius="md"
+        width="50vw"
+        maxWidth="600px"
+        mx="auto"
+        mt={10}
+      >
+        <Heading as="h2" size="lg" mb={4} textAlign="center">
+          Login
+        </Heading>
+        <Heading as="h3" size="md" mb={4} textAlign="center">
+          Sign in as:
+        </Heading>
+        <RadioGroup
+          onChange={(value) => {
+            setUserIsRootUser(value === "root");
+          }}
+          value={userIsRootUser ? "root" : "account"}
+        >
+          <Stack spacing={4}>
+            <HStack justify="center">
+              <Radio value="root">Root User</Radio>
+              <Radio value="account" selected>
+                Account User
+              </Radio>
+            </HStack>
+          </Stack>
+        </RadioGroup>
+
+        <FormControl isRequired mt={4}>
+          <FormLabel>Email</FormLabel>
+          <Input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
+          />
+        </FormControl>
+
+        <FormControl isRequired mt={4}>
+          <FormLabel>Password</FormLabel>
+          <Input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter your password"
+          />
+        </FormControl>
+
+        <Button colorScheme="green" mt={4} onClick={handleSubmit} width="full">
+          Submit
+        </Button>
+
+        <Divider my={4} />
+
+        <Button colorScheme="blue" onClick={handleChangePassword} width="full">
+          Change Password
+        </Button>
+      </Box>
+    </>
+  );
 }
 
 export default Login
