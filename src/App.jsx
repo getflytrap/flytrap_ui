a1ba1985b8c19f78c04e8e060c2c10b3da61c359
Saul <saulthompson@users.noreply.github.com>
2024-11-07 09:31:00 +0000

    feat: Add React Router with basic routing
    Co-authored-by: Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Heading } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <Heading>Hello!</Heading>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Heading>Hello!</Heading>} />
+        <Route path="/login" element={<Heading>Login Page</Heading>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
