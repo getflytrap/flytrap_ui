9f500d8e05dfc0fa39888dae51af529021f6dbba
Saul <saulthompson@users.noreply.github.com>
2024-11-07 09:32:18 +0000

    feat: Integrate auth context and hook
    Co-authored-by: Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState, createContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Heading } from "@chakra-ui/react";
-import "./App.css";
+import { useAuth } from "./hooks/auth-hook";
+import { AuthContext } from "./contexts/auth-context";
 
 function App() {
+  const { token, login, logout } = useAuth();
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Heading>Hello!</Heading>} />
-        <Route path="/login" element={<Heading>Login Page</Heading>} />
-      </Routes>
-    </BrowserRouter>
+    <AuthContext.Provider value={{ isLoggedIn: !!token, token, login, logout }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Heading>Hello!</Heading>} />
+          <Route path="/login" element={<Heading>Login Page</Heading>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
