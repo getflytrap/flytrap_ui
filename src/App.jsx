3fbd43cb1617ddbdbe3980229b39fd1dd8ed54ce
Saul <saulthompson@users.noreply.github.com>
2024-11-07 09:36:27 +0000

    feat: Import various components and add routes for each
    Co-authored-by: Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,68 @@
-import React, { useState, createContext } from "react";
+import React, { useState, useEffect, createContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Heading } from "@chakra-ui/react";
-import { useAuth } from "./hooks/auth-hook";
-import { AuthContext } from "./contexts/auth-context";
+import { Center, Spinner } from "@chakra-ui/react";
 
-function App() {
+import { AuthContext } from "./contexts/auth-context";
+import { useAuth } from "./hooks/auth-hook";
+import ErrorDashboardLayout from "./errors/pages/ErrorDashboardLayout";
+import ErrorDashboard from "./errors/pages/ErrorDashboard";
+import ErrorDetails from "./error-details/pages/ErrorDetails";
+import Login from "./authentication/pages/Login";
+import WarningModal from "./shared/WarningModal";
+import RootConsole from "./root-console/pages/RootConsole";
+import ManageUsers from "./root-console/pages/ManageUsers";
+import AuthRequired from "./authentication/components/AuthRequired";
+import MainLayout from "./shared/MainLayout";
+import Projects from "./projects/pages/Projects";
+import "./App.css";
+
+const App = () => {
   const { token, login, logout } = useAuth();
+
+  const [selectedProject, setSelectedProject] = useState();
 
   return (
     <AuthContext.Provider value={{ isLoggedIn: !!token, token, login, logout }}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Heading>Hello!</Heading>} />
-          <Route path="/login" element={<Heading>Login Page</Heading>} />
+          <Route path="/login" element={<Login />} />
+
+          <Route element={<AuthRequired token={token} />} />
+          <Route element={<MainLayout />}>
+            <Route
+              path="/"
+              element={<Projects setSelectedProject={setSelectedProject} />}
+            />
+            <Route
+              path="/errors"
+              element={
+                <ErrorDashboardLayout
+                  selectedProject={selectedProject}
+                  setSelectedProject={setSelectedProject}
+                />
+              }
+            >
+              <Route
+                index
+                element={<ErrorDashboard selectedProject={selectedProject} />}
+              />
+              <Route
+                path=":id"
+                element={
+                  <ErrorDetails
+                    selectedProject={selectedProject}
+                    setSelectedProject={setSelectedProject}
+                  />
+                }
+              />
+            </Route>
+            <Route path="/root-console" element={<RootConsole />} />
+            <Route path="/manage-users" element={<ManageUsers />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </AuthContext.Provider>
   );
-}
+};
 
 export default App;
