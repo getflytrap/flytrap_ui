028efb88987ee4a24478c92c9be4e077b014d304
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2024-11-09 12:20:51 -0500

    feat: styling main layout to be responsive and grouping filter bar buttons together
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>

--- src/errors/components/FilterBar.jsx
+++ src/errors/components/FilterBar.jsx
@@ -14,12 +14,15 @@ const FilterBar = ({
     setSelectedHandled(value);
   };
 
+  const fixedButtonWidth = "90px";
+
   return (
     <Box p={4} bg="gray.100">
       <Flex alignItems="center">
         <Select
           placeholder="Select time period"
           onChange={handleTimeChange}
+          bg="gray.200"
           width="200px"
         >
           <option value="Today">Today</option>
@@ -32,28 +35,40 @@ const FilterBar = ({
 
         <Box mx={4} />
 
-        <Stack direction="row" spacing={0}>
-          {["All", "Handled", "Unhandled"].map((option) => (
-            <Box
-              key={option}
-              as="button"
-              onClick={() => handleResolvedChange(option)}
-              borderRadius="20px"
-              p={2}
-              bg={selectedHandled === option ? "blue.500" : "transparent"}
-              color={selectedHandled === option ? "white" : "black"}
-              border={
-                selectedHandled === option
-                  ? "2px solid blue"
-                  : "2px solid transparent"
-              }
-              transition="background 0.2s, color 0.2s, border 0.2s"
-              _hover={{ bg: "blue.300", color: "white" }}
-            >
-              <Text>{option}</Text>
-            </Box>
-          ))}
-        </Stack>
+        <Box borderRadius="50px" bg="gray.200" p="4px">
+          <Stack direction="row" spacing={6}>
+            {["All", "Handled", "Unhandled"].map((option) => (
+              <Box
+                key={option}
+                as="button"
+                onClick={() => handleResolvedChange(option)}
+                borderRadius="20px"
+                p={2}
+                width={fixedButtonWidth}
+                bg={selectedHandled === option ? "blue.500" : "transparent"}
+                color={selectedHandled === option ? "white" : "black"}
+                border={
+                  selectedHandled === option
+                    ? "2px solid blue"
+                    : "2px solid transparent"
+                }
+                transition="background 0.2s, color 0.2s, border 0.2s"
+                _hover={{
+                  bg: "blue.300",
+                  color: "white",
+                }}
+                _focus={{
+                  outline: "none",
+                }}
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Text>{option}</Text>
+              </Box>
+            ))}
+          </Stack>
+        </Box>
       </Flex>
     </Box>
   );
