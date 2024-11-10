0f6154a1c50e15250196eea8e05abe4644a50a5a
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-10 16:47:26 +0100

    feat: added Error types
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,26 @@
-export type Project = {
+export interface Project {
   uuid: string;
   name: string;
   issue_count: number;
 };
+
+export interface ErrorData {
+  uuid: string;
+  name: string;
+  message: string;
+  created_at: string;
+  line_number: number;
+  col_number: number;
+  stack_trace?: string;
+  handled: boolean;
+  resolved: boolean;
+}
+
+// export interface ErrorResponse {
+//   errors: any[]; // You may want to replace `any[]` with a proper type for the error data
+//   currentPage: number;
+//   totalPages: number;
+// }
+
+export type HandledFilter = "All" | "Handled" | "Unhandled";
+export type TimeFilter = "Today" | "Last 7 days" | "Last 14 days" | "Last 30 days" | "Last 90 days" | "Forever";
