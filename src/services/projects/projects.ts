149ace69429fab4a31a921c8b11d1a3f520ce29a
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-12 18:20:42 +0100

    feat: updated createProject service to send platform
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- src/services/projects/projects.ts
+++ src/services/projects/projects.ts
@@ -8,10 +8,11 @@ export const getAllProjects = async (): Promise<getAllProjectsResponse> => {
 };
 
 export const createProject = async (
-  projectName: string,
+  projectName: string, platform: string
 ): Promise<createProjectResponse> => {
   const { data } = await apiClient.post("/api/projects", {
     name: projectName,
+    platform
   });
 
   return data;
