d51670c4ae058d25e0b16d1402489ceff7b918c0
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-12 13:37:40 +0100

    Revert "feat: update ErrorRow component to support both ErrorData and Rejection types"
    This reverts commit 4a69415639a1d6770517d4608c2b2839dfaad998.

--- src/errors/components/ErrorRow.tsx
+++ src/errors/components/ErrorRow.tsx
@@ -4,51 +4,47 @@ import { ErrorData, Rejection } from "../../types";
 import { Project } from "../../types";
 
 interface ErrorRowProps {
-  issue: ErrorData | Rejection;
+  error: ErrorData | Rejection;
   selectedProject: Project | null;
   selectedHandled: string;
   selectedTime: string;
 }
 
 const ErrorRow = ({
-  issue,
+  error,
   selectedProject,
   selectedHandled,
   selectedTime,
 }: ErrorRowProps) => {
-  const isErrorData = (issue: ErrorData | Rejection): issue is ErrorData => {
-    return "name" in issue;
-  };
-
   return (
-    <Box as="tr" key={issue.uuid} mb={2}>
+    <Box as="tr" key={error.uuid} mb={2}>
       <Td>
         <Link
           as={RouterLink}
-          to={`/projects/${selectedProject?.uuid}/errors/${issue.uuid}`}
+          to={`/projects/${selectedProject?.uuid}/errors/${error.uuid}`}
           _hover={{ color: "blue.500", cursor: "pointer" }}
           state={{ time: selectedTime, handled: selectedHandled }}
         >
-          {isErrorData(issue) ? issue.name || "Un-named Error" : issue.value}
+          {error.name || "Un-named Error"}
         </Link>
       </Td>
-      {/* <Td>{issue.type}</Td> */}
+      {/* <Td>{error.type}</Td> */}
       <Td>
         <Box
           as="span"
           borderRadius="20px"
-          bg={issue.handled ? "green.50" : "red.50"}
-          border={`1px solid ${issue.handled ? "green.800" : "red.800"}`}
-          color={issue.handled ? "green.800" : "red.800"}
+          bg={error.handled ? "green.50" : "red.50"}
+          border={`1px solid ${error.handled ? "green.800" : "red.800"}`}
+          color={error.handled ? "green.800" : "red.800"}
           px={2}
           py={1}
           display="inline-block"
         >
-          {issue.handled ? "Handled" : "Unhandled"}
+          {error.handled ? "Handled" : "Unhandled"}
         </Box>
       </Td>
-      <Td>{new Date(issue.created_at).toLocaleString()}</Td>
-      <Td>{issue.resolved ? "Resolved" : "Unresolved"}</Td>
+      <Td>{new Date(error.created_at).toLocaleString()}</Td>
+      <Td>{error.resolved ? "Resolved" : "Unresolved"}</Td>
     </Box>
   );
 };
