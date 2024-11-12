858b1d6d933d603f39d60d7ee1b1f1c64d697efe
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2024-11-12 10:48:54 -0500

    feat: added dropdown menu button
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>

--- src/shared/MainLayout.tsx
+++ src/shared/MainLayout.tsx
@@ -1,11 +1,22 @@
 import { Outlet, useNavigate, Link, useLocation } from "react-router-dom";
-import { Box, Button, Flex, Heading, Image } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Text,
+} from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 import { useAuth } from "../hooks/useAuth";
-import flytrap_logo from '../assets/flytrap_logo.png'
-
+import flytrap_logo from "../assets/flytrap_logo.png";
 
 const MainLayout = () => {
-  const { logout } = useAuth();
+  const { logout, user } = useAuth();
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -35,11 +46,10 @@ const MainLayout = () => {
         justify="space-between"
         padding="1rem"
         whiteSpace="nowrap"
-        alignItems="center"      
+        alignItems="center"
       >
-        <Heading size="2xl" textAlign="center" >
-        <Image src={flytrap_logo} alt="Flytrap Logo" height="150px" />
-
+        <Heading size="2xl" textAlign="center">
+          <Image src={flytrap_logo} alt="Flytrap Logo" height="150px" />
         </Heading>
         <Flex justifyContent="flex-end" alignItems="center" marginTop="1rem">
           <Link to="/">
@@ -47,16 +57,59 @@ const MainLayout = () => {
               Projects
             </Button>
           </Link>
-          <Link to="/root-console">
-            <Button colorScheme={getButtonColor("/root-console")} mr="10px">
-              Admin Console
-            </Button>
-          </Link>
+
+          <Menu>
+            <MenuButton
+              as={Button}
+              colorScheme="gray"
+              rightIcon={<ChevronDownIcon />}
+              mx="10px"
+            >
+              Account
+            </MenuButton>
+            <MenuList
+              backgroundColor="white"
+              boxShadow="lg"
+              borderRadius="8px"
+              padding="1rem"
+              width="200px"
+              marginTop="10px"
+            >
+              <MenuItem isDisabled>
+                <Text fontSize="sm">
+                  Signed in as {user?.username || "Guest"}
+                </Text>
+              </MenuItem>
+              <Link to="/change-password">
+                <MenuItem
+                  backgroundColor="transparent"
+                  _hover={{ backgroundColor: "#f0f0f0" }}
+                  padding="10px"
+                  margin="5px 0"
+                >
+                  Change Password
+                </MenuItem>
+              </Link>
+              <Link to="/root-console">
+                <MenuItem
+                  backgroundColor="transparent"
+                  _hover={{ backgroundColor: "#f0f0f0" }}
+                  padding="10px"
+                  margin="5px 0"
+                >
+                  Admin Console
+                </MenuItem>
+              </Link>
+            </MenuList>
+          </Menu>
+
+          {/* Sign Out Button */}
           <Button onClick={handleSignOut} colorScheme="gray">
             Sign Out
           </Button>
         </Flex>
       </Flex>
+
       <Box
         padding="1rem"
         width="100%"
