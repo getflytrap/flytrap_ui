efc24c519216dfadf0694e14e24b7d04ba0566d8
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-12 17:02:04 +0100

    fix: added status property to GetIssueResponse type
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- src/services/projectsIssues/projectsIssuesTypes.ts
+++ src/services/projectsIssues/projectsIssuesTypes.ts
@@ -1,6 +1,7 @@
 import { ErrorData, Rejection } from "../../types/index";
 
 export interface GetIssuesResponse {
+  status: string,
   data: {
     issues: (ErrorData | Rejection)[];
     totalPages: number;
