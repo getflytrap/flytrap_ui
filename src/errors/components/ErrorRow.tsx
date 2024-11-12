9309dfb2dcb7f908b0419c3676b7b3d941bd398c
Clarissa Roeder <123587588+clarissaroeder@users.noreply.github.com>
2024-11-12 13:44:53 +0100

    feat: update ErrorRow component to support both ErrorData and Rejection types
    Co-authored-by: Rebecca Biancofiore <58274427+fenris55@users.noreply.github.com>
    Co-authored-by: Saul Thompson <118783042+saulthompson@users.noreply.github.com>
    Co-authored-by: Tony Kovatch <96496455+Akovatch@users.noreply.github.com>

--- src/errors/components/ErrorRow.tsx
+++ src/errors/components/ErrorRow.tsx
@@ -4,47 +4,53 @@ import { ErrorData, Rejection } from "../../types";
 import { Project } from "../../types";
 
 interface ErrorRowProps {
-  error: ErrorData | Rejection;
+  issue: ErrorData | Rejection;
   selectedProject: Project | null;
   selectedHandled: string;
   selectedTime: string;
 }
 
 const ErrorRow = ({
-  error,
+  issue,
   selectedProject,
   selectedHandled,
   selectedTime,
 }: ErrorRowProps) => {
+  const isErrorData = (issue: ErrorData | Rejection): issue is ErrorData => {
+    return "name" in issue;
+  };
+
   return (
-    <Box as="tr" key={error.uuid} mb={2}>
+    <Box as="tr" key={issue.uuid} mb={2}>
       <Td>
         <Link
           as={RouterLink}
-          to={`/projects/${selectedProject?.uuid}/errors/${error.uuid}`}
+          to={isErrorData(issue)
+            ? `/projects/${selectedProject?.uuid}/issues/errors/${issue.uuid}`
+            : `/projects/${selectedProject?.uuid}/issues/rejections/${issue.uuid}`}
           _hover={{ color: "blue.500", cursor: "pointer" }}
           state={{ time: selectedTime, handled: selectedHandled }}
         >
-          {error.name || "Un-named Error"}
+          {isErrorData(issue) ? issue.name || "Un-named Error" : issue.value}
         </Link>
       </Td>
-      {/* <Td>{error.type}</Td> */}
+      {/* <Td>{issue.type}</Td> */}
       <Td>
         <Box
           as="span"
           borderRadius="20px"
-          bg={error.handled ? "green.50" : "red.50"}
-          border={`1px solid ${error.handled ? "green.800" : "red.800"}`}
-          color={error.handled ? "green.800" : "red.800"}
+          bg={issue.handled ? "green.50" : "red.50"}
+          border={`1px solid ${issue.handled ? "green.800" : "red.800"}`}
+          color={issue.handled ? "green.800" : "red.800"}
           px={2}
           py={1}
           display="inline-block"
         >
-          {error.handled ? "Handled" : "Unhandled"}
+          {issue.handled ? "Handled" : "Unhandled"}
         </Box>
       </Td>
-      <Td>{new Date(error.created_at).toLocaleString()}</Td>
-      <Td>{error.resolved ? "Resolved" : "Unresolved"}</Td>
+      <Td>{new Date(issue.created_at).toLocaleString()}</Td>
+      <Td>{issue.resolved ? "Resolved" : "Unresolved"}</Td>
     </Box>
   );
 };
